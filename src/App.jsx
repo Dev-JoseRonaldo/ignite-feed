838e674af5fb3adc5451c600396dca975dfdd513
José Ronaldo <dev.joseronaldo@gmail.com>
2022-08-12 21:36:13 -0300

    :sparkles: feat: Estrutura e estilização do componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 import './global.css';
 
 export const App = () => {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,28 @@
+
+import styles from './styles.module.css';
+ 
+import Avatar from '../../assets/avatar.png'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1577648188599-291bb8b831c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img className='avatar' src={Avatar} />
+        
+        <strong>José Ronaldo</strong>
+        <span>Front-end Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
