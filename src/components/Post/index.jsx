0d22cacd97432882d0ad51d3573755c47cab1d1d
José Ronaldo <dev.joseronaldo@gmail.com>
2022-08-18 17:56:38 -0300

    :sparkles: Colocando os dados no posts dinamicamente e formatando datas

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,30 +1,43 @@
-import avatarImg from "../../assets/avatar.png"
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/esm/locale/pt-BR';
+
 import { Avatar } from "../Avatar";
 import { Comment } from "../Comment";
 
 import styles from "./styles.module.css";
 
-export const Post = () => {
+export const Post = ({author, publishedAt, content}) => {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH'h':mm'm'", {
+    locale:ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
+    locale:ptBR,
+    addSuffix:true,
+  })
 
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src={avatarImg} alt="" />
+          <Avatar src={author.avatarUrl} alt="" />
           <div className={styles.authorInfo}>
-            <strong>José Ronaldo</strong>
-            <span>Frontend Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="16 de agosto ás 08:13h" dataTime="2022-08-16 08:13:30">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dataTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
       </header>
 
       <div className={styles.content}>
-       <p>Fala galeraa 👋</p>
-       <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-       <p>👉 jane.design/doctorcare</p>
-       <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+        {content.map(line => {
+          if(line.type === 'paragraph'){
+            return <p>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }         
+        })}
       </div>
 
       <form  className={styles.commentForm}>
