8bbbfe8675d2e77527e3195037906c43213f94c3
José Ronaldo <dev.joseronaldo@gmail.com>
2022-08-20 19:58:36 -0300

    :lipstick: feat: Estilizaçao CSS do modal de deletar comentários

--- src/components/DeleteCommentModal/index.tsx
+++ src/components/DeleteCommentModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from "react-modal"
 
+
+
 interface DeleteCommentModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -8,8 +10,18 @@ interface DeleteCommentModalProps {
 export const DeleteCommentModal = ({ isOpen, onRequestClose }: DeleteCommentModalProps) => {
 
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Modalzin</h2>
+    <Modal 
+      isOpen={isOpen} 
+      onRequestClose={onRequestClose}
+      overlayClassName = "react-modal-overlay"
+      className="react-modal-content"
+    >
+      <h2>Excluir comentário</h2>
+      <p>Você tem certeza que gostaria de excluir este comentário?</p>
+      <div className="react-modal-btn-box">
+        <button className="react-modal-btn-cancel">Cancelar</button>
+        <button className="react-modal-btn-confirm">Sim, excluir</button>
+      </div>
     </Modal>
   )
 }
